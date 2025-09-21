3f481bb88047687e502a02bf3943f8958a068d8e
M3fjuLeo <mateuszropek@onet.pl>
2025-09-21 18:40:33 +0200

    adding userSchema

---
+++ backend/src/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = required("mongoose");
+const bcrypt = required("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: [true, "Please tell us your name"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minLength: 8,
+  },
+  avatar: {
+    type: String,
+    default: null,
+  },
+  timestamps: true,
+});
+
+userSchema.pre("save", async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified("password")) return next();
+
+  // Hash the password with cost 12
+  this.password = await bcrypt.hash(this.password, 12);
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
