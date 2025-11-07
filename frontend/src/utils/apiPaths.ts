cf1318471713e90a0f990f4bb9092ad113a99ee7
M3fjuLeo <mateuszropek@onet.pl>
2025-11-07 11:27:26 +0100

    Set API baseURL for production

--- frontend/src/utils/apiPaths.ts
+++ frontend/src/utils/apiPaths.ts
@@ -1,4 +1,6 @@
-export const BASE_URL = "http://localhost:3001";
+// export const BASE_URL = "http://localhost:3001";
+export const BASE_URL =
+  "https://mern-expense-tracker-app-29fw.onrender.com/api/v1";
 
 export const API_PATHS = {
   AUTH: {
