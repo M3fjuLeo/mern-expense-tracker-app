88b8f93cd5977bb0057446406284abbd0ace206f
M3fjuLeo <mateuszropek@onet.pl>
2025-11-07 12:31:32 +0100

    Fix type handling in Expense

--- frontend/src/pages/Dashboard/Expense.tsx
+++ frontend/src/pages/Dashboard/Expense.tsx
@@ -24,7 +24,7 @@ const Expense = () => {
 
   const queryClient = useQueryClient();
 
-  const onSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (!title || !amount || !date) {
